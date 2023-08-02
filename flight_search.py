151e33508276f8d33344681fb540b574242bfb39
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-08-02 19:13:19 +0800

    Add Cheap Flight Finder

--- flight_search.py
+++ flight_search.py
@@ -1,24 +1,63 @@
 import os
-
 import requests
+from datetime import datetime, timedelta
+from flight_data import FlightData
 
 
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
     def __init__(self):
-        self.flight_search_endpoint = 'https://api.tequila.kiwi.com'
-        self.flight_search_api_key = '5DyoV7Vy4vpc1udpKm2RliiCFmiUOh81'
+        self.starting_city = 'LON'
+        self.curr = 'GBP'
+        self.flight_search_endpoint_search = f"https://api.tequila.kiwi.com/v2/search"
+        self.flight_search_endpoint_locations = f"https://api.tequila.kiwi.com/locations/query"
+        self.headers = {
+            "apikey": os.environ['FLIGHT_SEARCH_API_KEY']
+        }
+
+    def get_flights(self, departure_city_code) -> FlightData:
+        date_today = datetime.now()
+        tomorrow = (date_today + timedelta(days=1)).strftime("%d/%m/%Y")
+        six_months = (date_today + timedelta(days=180)).strftime("%d/%m/%Y")
+
+        parameters = {
+            "fly_from": self.starting_city,
+            "fly_to": departure_city_code,
+            "date_from": tomorrow,
+            "date_to": six_months,
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "max_stopovers": 0,
+            "curr": self.curr,
+            "one_for_city": 1,
+        }
+
+        response = requests.get(self.flight_search_endpoint_search, params=parameters, headers=self.headers)
+        try:
+            data = response.json()['data'][0]
+        except IndexError:
+            print(f"No flights found for {departure_city_code}")
+            return None
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            depart_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+        )
+        print(f"{flight_data.destination_city}: £{flight_data.price}")
+        return flight_data
 
     def get_iata_code(self, city):
-        flight_search_endpoint_locations = f"{self.flight_search_endpoint}/locations/query"
-        headers = {
-            "apikey": self.flight_search_api_key
-        }
         parameters = {
             "term": city,
         }
 
-        response = requests.get(url=flight_search_endpoint_locations, params=parameters, headers=headers)
+        response = requests.get(url=self.flight_search_endpoint_locations, params=parameters, headers=self.headers)
         location_data = response.json()
         iata_code = location_data['locations'][0]['code']
 
--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 from data_manager import DataManager
 from flight_search import FlightSearch
+from flight_data import FlightData
 from pprint import pprint
 
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the
 # program requirements.
 
+# Load Classes
 data_manager = DataManager()
 flight_search = FlightSearch()
 
-sheet_data = data_manager.sheet_data
-pprint(sheet_data)
+# Load Google Sheets Data
+# sheet_data = data_manager.sheet_data
+# pprint(sheet_data)
+sheet_data = [{'city': 'Paris', 'iataCode': 'PAR', 'id': 2, 'lowestPrice': 100},
+ {'city': 'Berlin', 'iataCode': 'BER', 'id': 3, 'lowestPrice': 42},
+ {'city': 'Tokyo', 'iataCode': 'TYO', 'id': 4, 'lowestPrice': 485},
+ {'city': 'Sydney', 'iataCode': 'SYD', 'id': 5, 'lowestPrice': 551},
+ {'city': 'Istanbul', 'iataCode': 'IST', 'id': 6, 'lowestPrice': 95},
+ {'city': 'Kuala Lumpur', 'iataCode': 'KUL', 'id': 7, 'lowestPrice': 414},
+ {'city': 'New York', 'iataCode': 'NYC', 'id': 8, 'lowestPrice': 240},
+ {'city': 'San Francisco', 'iataCode': 'SFO', 'id': 9, 'lowestPrice': 260},
+ {'city': 'Cape Town', 'iataCode': 'CPT', 'id': 10, 'lowestPrice': 378}]
 
+
+# Get and Replace missing iata Code
 for data in sheet_data:
     if data['iataCode'] == '':
         data['iataCode'] = flight_search.get_iata_code(data['city'])
         data_manager.edit_iata_code(data['id'], data['iataCode'])
+
+flight_data = []
+
+for data in sheet_data:
+    flight_data = flight_search.get_flights(data['iataCode'])
+
+
